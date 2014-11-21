85557683fe148c9d23d595d0e7edbc5318284318
Aaron Craelius <aaroncraelius@gmail.com>
2014-11-21 15:53:27 -0500

    Initial import from freactive.

---
+++ README.md
@@ -0,0 +1,7 @@
+# freactive.core
+
+Generic reactive atoms, expressions, cursors for Clojure(Script).
+
+## License
+
+Distributed under the Eclipse Public License, either version 1.0 or (at your option) any later version.
---
+++ src/clojure/freactive/macros.clj
@@ -0,0 +1,43 @@
+(ns freactive.macros)
+
+(defmacro rx [& body]
+  `(freactive.core/rx*
+     (fn []
+       ~@body)))
+
+(defmacro rx [& body]
+  `(freactive.core/rx*
+     (fn []
+       ~@body)
+     false))
+
+(defmacro lazily [& body]
+  `(binding [freactive.core/*lazy* true]
+     ~@body))
+
+(defmacro eagerly [& body]
+  `(binding [freactive.core/*lazy* false]
+     ~@body))
+
+(defmacro non-reactively [& body]
+  `(binding [freactive.core/*invalidate-rx* nil]
+     ~@body))
+
+(defmacro debug-rx [rx]
+  (let [dbg-str (str "rx-debug" (pr-str rx))]
+    `(let [dbg-str# ~dbg-str
+           res#
+           (binding [freactive.core/*do-trace-captures*
+                     (fn
+                       ([] (println dbg-str# ": starting capture"))
+                       ([c#] (println dbg-str# "captured :" c#)))]
+             ~rx)
+           invalidation-cb#
+           (fn [k# r#] (println dbg-str#
+                               "notifiying invalidation watches:"
+                               (cljs.core/keys (.-invalidation-watches res#))
+                               "& watches:"
+                               (cljs.core/keys (.-watches res#))))]
+       (freactive.core/add-invalidation-watch res# dbg-str# invalidation-cb#)
+       res#)))
+
