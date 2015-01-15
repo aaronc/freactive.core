8b3e73b8208f2ba18fad591c6757c739b6488d75
Aaron Craelius <aaroncraelius@gmail.com>
2015-01-14 19:44:36 -0500

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "https://github.com/aaronc/freactive.core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies
+  [[org.clojure/clojure "1.7.0-alpha3"]]
   :profiles
   {:dev
     {:plugins [[com.cemerick/austin "0.1.5"]]
      :dependencies 
-     [[org.clojure/clojure "1.7.0-alpha3"]
-      [org.clojure/clojurescript "0.0-2371"]
+     [[org.clojure/clojurescript "0.0-2371"]
       [com.cemerick/clojurescript.test "0.3.1"]]}}
   :source-paths ["src-clj" "src-cljs"]
   :javac-options ["-Xlint:unchecked"]
