9becf9a6ada055a9b8509cfbbe340c57827e0c6a
Aaron Craelius <aaroncraelius@gmail.com>
2015-06-02 19:17:24 -0400

    Refactoring

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject freactive.core "0.2.0-alpha1"
+(defproject freactive.core "0.2.0-SNAPSHOT"
   :description "Generic reactive atoms, expressions, cursors"
   :url "https://github.com/aaronc/freactive.core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies
-  [[org.clojure/clojure "1.7.0-RC1"]]
   :profiles
   {:dev
    {:dependencies 
-     [[org.clojure/clojurescript "0.0-3269"]]}}
+    [[org.clojure/clojure "1.7.0-RC1"]
+     [org.clojure/clojurescript "0.0-3269"]]}}
   :source-paths ["src/clojure"]
   :javac-options ["-Xlint:unchecked"]
   :java-source-paths ["src/java"])
