c9ea27dc98a3a0c8d94a1de4bf0c436c3826df34
Aaron Craelius <aaroncraelius@gmail.com>
2015-07-09 23:04:39 -0400

    Cleaned up comments

--- src/clojure/freactive/macros.clj
+++ src/clojure/freactive/macros.clj
@@ -42,26 +42,6 @@
     (fn [~bind-sym] ~body)
     ~keyset-cursor ~opts))
 
-;; (defmacro defsubtype [t fields supertype & impls]
-;;   (let [env &env
-;;         r (:name (cljs.analyzer/resolve-var (dissoc env :locals) t))
-;;         impls (cons 'Object impls)]
-;;     `(do
-;;        (deftype* ~t ~fields nil nil)
-;;        (let [tmp# (fn [])]
-;;          (set! (.-prototype tmp#) (.-prototype ~supertype))
-;;          (set! (.-prototype ~t) (new tmp#)))
-;;        (extend-type ~t ~@(cljs.core/dt->et t impls fields))
-;;        (set! (.-getBasis ~t) (fn [] '[~@fields]))
-;;        (let [basis# (set (.getBasis ~t))]
-;;          (doseq [pf# (.getBasis ~supertype)]
-;;            (assert (contains? basis# pf#) "Sub-types fields not compatible with parent fields")))
-;;        (set! (.-cljs$lang$type ~t) true)
-;;        (set! (.-cljs$lang$ctorStr ~t) ~(str r))
-;;        (set! (.-cljs$lang$ctorPrWriter ~t) (fn [this# writer# opt#] (cljs.core/-write writer# ~(str r))))
-;;        ~t)))
-
-
 (defmacro defsubtype [t fields supertype & impls]
   (let [env &env
         r (:name (cljs.analyzer/resolve-var (dissoc env :locals) t))
@@ -69,15 +49,4 @@
     `(do
        (deftype ~t ~fields ~@impls)
        (freactive.util/inherit! ~t ~supertype)
-       ;; (if (.-setPrototypeOf js/Object)
-       ;;   (.setPrototypeOf js/Object (.-prototype ~t) (.-prototype ~supertype))
-       ;;   (goog.object/forEach
-       ;;    (.-prototype ~supertype)
-       ;;    (let [proto# (.-prototype ~t)]
-       ;;      (fn [val# key# obj#]
-       ;;        (when-not (.hasOwnProperty proto# key#)
-       ;;          (aset proto# key# val#))))))
-       ;; (let [basis# (set (.getBasis ~t))]
-       ;;   (doseq [pf# (.getBasis ~supertype)]
-       ;;     (assert (contains? basis# pf#) "Sub-types fields not compatible with parent fields")))
        ~t)))
